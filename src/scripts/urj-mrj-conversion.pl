6cab04d42e0cb75b365cf9c48f05a70bfb63b8a6
Jack Rueter <rueter.jack@gmail.com>
2023-05-25 21:01:06 +0300

    add urj-mrj-conversion.pl
    comment out a couple of lexicons

---
+++ src/scripts/urj-mrj-conversion.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl -w
+#
+# urj-mrj-conversion.pl
+# Convert names in the URJ propernoun lexicon to mrj.
+
+use strict;
+use utf8;
+use feature 'unicode_strings';
+BEGIN {
+       $| = 1;
+       binmode(STDIN, ':encoding(UTF-8)');
+       binmode(STDOUT, ':encoding(UTF-8)');
+}
+use open qw( :encoding(UTF-8) :std );
+
+while(<>) {
+	# Continuation lexicon substitutions:
+#	s/ C-FI-NEN/nen LONDON/g ;
+	s/ PROP-PLC_KEL1/ N_01/g ;
+	s/ PROP-PLC_VELE/ N_01/g ;
+	s/ PROP_VELE/ N_01/g ;
+
+	
+# loanwords with compound border over identical vowels
+	s/Hjarteelva/Hjarte-elva/g ;
+	
+
+# names with Inari Saami inflection
+	s/^Aanaar\+/!Aanaar+/g ;
+
+# sme special symbols
+	s/b9/b/g ;
+
+	my $line = $_;
+
+	print $line;
+}
+
