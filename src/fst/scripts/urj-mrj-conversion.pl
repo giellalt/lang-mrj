88ba1d9c4e1a6952134b6c26fbd0334ad00ad9a7
Jack Rueter <rueter.jack@gmail.com>
2024-08-19 13:59:20 +0300

    Correction to conversion generation

--- src/fst/scripts/urj-mrj-conversion.pl
+++ src/fst/scripts/urj-mrj-conversion.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 #
-# urj-mrj-conversion.pl
+# urj-Cyrl-mrj-conversion.pl
 # Convert names in the URJ propernoun lexicon to mrj.
 
 use strict;
@@ -18,14 +18,20 @@ while(<>) {
 #	s/ C-FI-NEN/nen LONDON/g ;
 	s/ PROP-PLC_KEL1/ N_01/g ;
 	s/ PROP-PLC_VELE/ N_01/g ;
-	s/ PROP_VELE/ N_01/g ;
-	s/ PROP_KAL/ N_01/g ;
 	s/ PROP_KAL_PLC/ N_01/g ;
-	s/ PROP_KUDO/ N_01/g ;
 	s/ PROP_KUDO_PLC/ N_01/g ;
-	s/ PROP_OSH/ N_01/g ;
 	s/ PROP_OSH_PLC/ N_01/g ;
+	s/ PROP_KAL_FEM/ N_01/g ;
+	s/ PROP_VELE_FEM/ PROP_KUDO_FEM/g ;
+	s/ PROP_VELE_MAL/ N_KUDO_MAL/g ;
+	s/ PROP_KAL / N_01 /g ;
+	s/ PROP_VELE / N_01 /g ;
+	s/ PROP_KUDO / N_01 /g ;
+	s/ PROP_OSH / N_01 /g ;
+	s/ PROP_KENKSH / N_01 /g ;
 
+# names with in Russian ия: Абхазия
+        s/^([^+:]*и)я(\+N\+Prop\+Sem\/Plc:)\1я\ PROP_RUS_JA/$1$2$1\ N_AEZAE/g ;
 	
 # loanwords with compound border over identical vowels
 	s/Hjarteelva/Hjarte-elva/g ;
