1070ecca100f9df013b946e3abdef2f328f92453
Jack Rueter <rueter.jack@gmail.com>
2024-09-02 11:24:17 +0300

    Edit conversion script for work with
    urj-Cyrl proper nouns

--- src/fst/scripts/urj-mrj-conversion.pl
+++ src/fst/scripts/urj-mrj-conversion.pl
@@ -16,19 +16,19 @@ use open qw( :encoding(UTF-8) :std );
 while(<>) {
 	# Continuation lexicon substitutions:
 #	s/ C-FI-NEN/nen LONDON/g ;
-	s/ PROP-PLC_KEL1/ N_01/g ;
-	s/ PROP-PLC_VELE/ N_01/g ;
-	s/ PROP_KAL_PLC/ N_01/g ;
-	s/ PROP_KUDO_PLC/ N_01/g ;
-	s/ PROP_OSH_PLC/ N_01/g ;
-	s/ PROP_KAL_FEM/ N_01/g ;
+	s/ PROP-PLC_KEL1/ PROP_/g ;
+	s/ PROP-PLC_VELE/ PROP_/g ;
+	s/ PROP_KAL_PLC/ PROP_/g ;
+	s/ PROP_KUDO_PLC/ PROP_/g ;
+	s/ PROP_OSH_PLC/ PROP_/g ;
+	s/ PROP_KAL_FEM/ PROP_/g ;
 	s/ PROP_VELE_FEM/ PROP_KUDO_FEM/g ;
 	s/ PROP_VELE_MAL/ N_KUDO_MAL/g ;
-	s/ PROP_KAL / N_01 /g ;
-	s/ PROP_VELE / N_01 /g ;
-	s/ PROP_KUDO / N_01 /g ;
-	s/ PROP_OSH / N_01 /g ;
-	s/ PROP_KENKSH / N_01 /g ;
+	s/ PROP_KAL / PROP_ /g ;
+	s/ PROP_VELE / PROP_ /g ;
+	s/ PROP_KUDO / PROP_ /g ;
+	s/ PROP_OSH / PROP_ /g ;
+	s/ PROP_KENKSH / PROP_ /g ;
 
 # names with in Russian ия: Абхазия
         s/^([^+:]*и)я(\+N\+Prop\+Sem\/Plc:)\1я\ PROP_RUS_JA/$1$2$1\ N_AEZAE/g ;
